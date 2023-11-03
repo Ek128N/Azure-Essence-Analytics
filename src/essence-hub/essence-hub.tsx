e063f6983107e76817c721fb6e33dd93e1205fc3
EmilKirichenko <kirichienko.emil@gmail.com>
2023-11-03 23:23:47 +0700

    init commit

---
+++ src/essence-hub/essence-hub.tsx
@@ -0,0 +1,23 @@
+import "azure-devops-ui/Core/override.css";
+import "./essence-hub.scss";
+
+//import * as SDK from "azure-devops-extension-sdk";
+import { Page } from "azure-devops-ui/Page";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class Hub extends React.Component {
+  /*public componentDidMount() {
+    SDK.init();
+  }*/
+
+  public render(): JSX.Element {
+    return (
+      <Page className="flex-grow">
+        <h1>Hello World</h1>
+      </Page>
+    );
+  }
+}
+
+ReactDOM.render(<Hub />, document.getElementById("root"));

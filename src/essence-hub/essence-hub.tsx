1a30f3db192193afcf6242f6222e104f240d12a5
ErionFY <goncharov.timofej@gmail.com>
2023-11-18 15:33:04 +0700

    Json register Added

--- src/essence-hub/essence-hub.tsx
+++ src/essence-hub/essence-hub.tsx
@@ -91,6 +91,7 @@ function Hub() {
         primary={true}
         onClick={HandleProcessMigration}
       />
+      <JsonInput/>
     </Page>
   );
 }
--- src/essence-hub/jsonInput/JsonInput.tsx
+++ src/essence-hub/jsonInput/JsonInput.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {JSX, useState} from "react";
 import {Button} from "azure-devops-ui/Button";
 import JsonInputTemplate from "../modules/JsonInputTemplate";
-
+import * as SDK from "azure-devops-extension-sdk";
 
 export default function render(): JSX.Element {
     const [jsonData, setJsonData] = useState<[JsonInputTemplate]>([new JsonInputTemplate()]);
@@ -23,22 +23,26 @@ export default function render(): JSX.Element {
         reader.readAsText(file);
 
         reader.onload = function () {
-            console.log(reader.result);
+            let readRes = reader.result as string;
+            Parse(readRes);
         };
 
         reader.onerror = function () {
             console.log(reader.error);
         };
 
-        let json = reader.result as string;
-        let obj = JSON.parse(json);
+        function Parse(json: string) {
+            let obj = JSON.parse(json);
+            const template: [JsonInputTemplate] = Object.assign([new JsonInputTemplate], obj) as [JsonInputTemplate];
+
+            setJsonData(template as [JsonInputTemplate]);
+            SDK.register("EssenceData", template);
+
+            //console.log(jsonData[0].alphas);
+            //console.log(template);
+        }
 
 
-        const template: [JsonInputTemplate] = Object.assign([new JsonInputTemplate], obj) as [JsonInputTemplate];
-
-        setJsonData(template as [JsonInputTemplate]);
-
-        console.log(jsonData);
     }
 
 
